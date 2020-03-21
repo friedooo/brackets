b00894a608fcfdad67152131c269c0c1e99c8e1f
friedooo <friedlich3@gmail.com>
2020-03-21 20:09:38 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let arr = str.split('');
+
+    let closeBr = [];
+    let openBr = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++)
+    {
+        openBr.push(bracketsConfig[i][0]);
+        closeBr.push(bracketsConfig[i][1]);
+    }
+
+   
+
+    console.log(openBr);
+    console.log(closeBr);
+
+    for (let i = arr.length-1; i >= 0; i--)
+    {
+        
+        for (let j = 0; j < openBr.length; j++)
+        {
+            if (arr[i] == openBr[j])
+            {
+                if (arr[i+1] == closeBr[j])
+                {
+                    
+                    arr.splice(i,2);
+                    console.log(arr);
+                    i = arr.length-1;
+                }
+            }
+        }
+    }
+
+    if (arr.length == 0)
+    return true;
+    else
+    return false;
 }
